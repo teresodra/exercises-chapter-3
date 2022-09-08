9caf73e3ddb13b9e2790900ee5b1f396cdab4cb4
Tereso del Río <delriot@uni.coventry.ac.uk>
2022-09-08 18:13:57 +0100

    First attemp of exercise 3.3

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -50,3 +50,47 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+    def __mul__(self, other):
+
+        if isinstance(other, Polynomial):
+            coefs = []
+            for i in range(self.degree() + other.degree() + 1):
+                initial_use_deg = tuple([] for _ in range(i+1))
+                self_use_deg = initial_use_deg + self.coefficients()[:i+1]
+                other_use_deg = initial_use_deg + other.coefficients()[:i+1]
+                new_coeff = 0
+                for j in range(i+1):
+                    new_coeff += self_use_deg[j] * other_use_deg[-j]
+                coefs.append(new_coeff)
+
+            return Polynomial(tuple(coefs))
+
+        elif isinstance(other, Number):
+            return Polynomial(tuple(elem * other for elem in self.coefficients))
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __sub__(self, other):
+        return self + (-1) * other
+
+    def __rsub__(self, other):
+        return (-1) * self + other
+
+    def __pow__(self, other):
+
+        if isinstance(other, int) and other >= 0:
+            power = 1
+            for _ in range(other):
+                power *= self
+            return power
+        
+        else: 
+            return NotImplemented
+
+    def __call__(self, scalar):
+        return sum(scalar * coef ^ i for i, coef in enumerate(self.coefficients))
